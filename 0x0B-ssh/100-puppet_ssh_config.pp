a0fc7d240c2c60732d0d31bd3a9e3ddbec603989
realdezzy <realdezzy1@gmail.com>
2023-03-30 00:02:03 -0700

    Puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,16 @@
-file { '/root/.ssh/config' :
-  ensure  => file,
-  content => "
-      Host realdezzy.tech
-        User ubuntu
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-        PubkeyAuthentication yes
-        StrictHostKeyChecking no
-        "
+# Setting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
