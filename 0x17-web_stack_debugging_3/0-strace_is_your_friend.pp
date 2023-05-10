5428e31f683d237bdbd0ab42d6021a16abb12980
realdezzy <realdezzy1@gmail.com>
2023-05-10 07:29:20 -0700

    puppet code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+#Create index.html file
+file {'/var/www/html/index.html':
+        ensure  => file,
+        mode    => '0644',
+        content => 'Hello World'
+}
+
+# Restart apache
+exec {'Apache restart':
+        command => 'sudo service apache2 restart'
+}
