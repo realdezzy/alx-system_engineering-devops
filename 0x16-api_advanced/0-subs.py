a2e36f2da12e4187e99de775b9646a02fae0e103
realdezzy <realdezzy1@gmail.com>
2023-05-09 20:18:49 -0700

    tasks 0 - 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Subscribers count
+"""Returns number of subscribers to a subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit: str):
+        """ Subreddit subscribers count """
+        count = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit))
+        obj = count.json()
+        subscribers = obj["data"]["subscribers"]
+        return subscribers
+
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Hot Posts
+"""Returns title of 10 hot posts for a subreddit"""
+import requests
+
+
+def top_ten(subreddit: str):
+        """ Hot post title """
+        count = requests.get("https://www.reddit.com/r/{}/top.json?limit=10"
+                        .format(subreddit))
+        obj = count.json()
+        children = obj["data"]["children"]
+
+        for i in len(children):
+                print(i["data"]["title"])
