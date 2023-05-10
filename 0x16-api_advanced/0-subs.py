29434cfd9ea971257623da7993eaf1db513334d2
realdezzy <realdezzy1@gmail.com>
2023-05-09 21:07:54 -0700

    return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit: str):
     """ Subreddit subscribers count """
-    count = requests.get("https://www.reddit.com/r/{}/about.json"
-                         .format(subreddit))
-    obj = count.json()
-    subscribers = obj["data"]["subscribers"]
-    return subscribers
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                         .format(subreddit),
+                         allow_redirects=False)
+    if response.status_code == 200:
+        obj = response.json()
+        subscribers = obj["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
