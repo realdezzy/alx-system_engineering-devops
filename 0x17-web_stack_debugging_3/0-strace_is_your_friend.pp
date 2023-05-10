e422c9b8bddc9dd4427585d23d1280431232488c
realdezzy <realdezzy1@gmail.com>
2023-05-10 08:19:34 -0700

    Wordpress Correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,9 @@
-#Create index.html file
-file {'/var/www/html/index.html':
-        ensure  => file,
-        mode    => '0644',
-        content => 'Hello World'
-}
-
-# Restart apache
-exec {'Apache restart':
-        command => 'sudo service apache2 restart',
-        path    => ['/usr/bin', '/usr/sbin',],
+# Debug wordpress
+exec { 'debug wordpress config':
+  environment => ['FILE=/var/www/html/wp-settings.php',
+                  'ERR=phpp',
+                  'CORRECT=php'],
+  command     => 'sudo sed -i "s/$ERR/$CORRECT/" $FILE',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
